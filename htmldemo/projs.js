7f542d6baecc46d70404e1047a15e58dba424bd0
zhaoshuzhen01 <zsz521@126.com>
2017-05-24 14:41:34 +0800

    /

--- htmldemo/projs.js
+++ htmldemo/projs.js
@@ -5,24 +5,65 @@ function setPage() {
     var ul = document.getElementById("titleul");
     var lis = ul.getElementsByTagName("li");
     $("#changeview").hide();
-    for (var i = 0;i <= lis.length - 1; i++) {
+    /* $(".anm_dog").animate({
+     left:'250px',
+     opacity:'1',
+     height:'150px',
+     width:'150px'
+     },3000,move_position());*/
+    for (var i = 0; i <= lis.length - 1; i++) {
         //alert(i+"");
-      lis[i].onmouseover = function(){
-          this.style.color = "red";
-          //this.style.backgroundColor = "white";
-          this.style.cursor = "pointer";
-          $("#changeview").show();
-      }
-        lis[i].onmouseout = function(){
+        lis[i].onmouseover = function () {
+            this.style.color = "red";
+            //this.style.backgroundColor = "white";
+            this.style.cursor = "pointer";
+            $("#changeview").show();
+        }
+        lis[i].onmouseout = function () {
             this.style.color = "white";
-           /* this.style.backgroundColor = "yellow"*/
+            /* this.style.backgroundColor = "yellow"*/
             time();
         };
     }
+   /*  img = document.getElementById("img");
+    img.style.top = yPos;
+    img.onmouseover = stop;
+    img.onmouseout =begin;*/
+    //start();
 }
-function hello(){
+function hello() {
     $("#changeview").hide();
 }
-function time(){
-    var t = window.setTimeout(hello,1000);
+function time() {
+    var t = window.setTimeout(hello, 1000);
 }
+
+
+function move_position() {
+    var number = Math.random();
+    number = Math.ceil(number * 100);
+    alert(number + "");
+    $(".anm_dog").animate({
+        left: '150px',
+        bottom: '150px'
+    }, 3000);
+}
+/*
+function begin() {
+    interval = setInterval('changePos()', delay)
+}
+function stop() {
+    clearInterval(interval);
+}
+function changePos() {
+    var number = Math.random();
+    number = Math.ceil(number * 1000);
+    $("#img").animate({
+        left:number,
+        top:number
+    },300);
+}
+function start() {
+    img.style.visibility = "visible";
+    interval = setInterval('changePos()', delay);
+}*/
